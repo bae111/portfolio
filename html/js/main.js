ebf41b70717f0424606966432ac50435b2110b55
unknown <sunmoneee@gmail.com>
2024-10-23 17:33:22 +0900

    feat: pellito 리뉴얼 작업

---
+++ html/js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+    $(window).on('scroll', function(){
+        if($(window).scrollTop() > 0){
+            $('#header').addClass('fix');
+        }else{
+            $('#header').removeClass('fix');
+        }
+    });
+});
