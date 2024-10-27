15dfc6de9c2791592849c541d41cc171c5270a44
bae111 <sunmoneee@gmail.com>
2024-10-27 23:41:49 +0900

    feat: portfolio 표지 작업

--- html/js/main.js
+++ html/js/main.js
@@ -1,4 +1,45 @@
+$(function () {
+    var mouseX, mouseY;
+    var ww = $(window).width();
+    var wh = $(window).height();
+    var traX, traY;
+    $(document).mousemove(function (e) {
+        mouseX = e.pageX;
+        mouseY = e.pageY;
+        traX = 4 * mouseX / 350 + 10;
+        traY = 4 * mouseY / 350 + 20;
+        $(".hd-title ").css({ "background-position": traX + "%" + traY + "%" });
+        $(".hd-title span").css({ "background-position": traY + "%" + traX + "%" });
+    });
+
+    $('.fall').each(function(index) {
+        $(this).css('--delay', `${index * 0.1}s`); // CSS 변수를 설정
+        $(this).addClass('fall'); // CSS 클래스를 추가하여 애니메이션 적용
+    });
+});
+
+
 gsap.registerPlugin(ScrollTrigger);
+
+// hd-title 애니메이션
+gsap.set(".hd-title", {
+    y: -100,
+    opacity: 0,
+});
+
+gsap.to(".hd-title", {
+    y: 0,
+    opacity: 1,
+    duration: 1,
+    ease: "power2.out",
+    stagger: 0.3, // 각 요소마다 0.3초 간격으로 애니메이션
+    scrollTrigger: {
+        trigger: "#header",
+        start: "top center",
+        end: "center",
+        markers: false 
+    }
+});
 
 // Company Section - Title
 gsap.set("#hard-coding .title, .detail", {
@@ -40,10 +81,6 @@ gsap.to(".hard-box", {
     } 
 });
 
-
-
-
-
 // Project section - content section
 gsap.set("#graphic-design", {
     x: -200,
@@ -67,21 +104,27 @@ gsap.to("#graphic-design", {
     } 
 });
 
-
 // Project section - Right
-gsap.set("#about", {  
-    x: 500 
+// gsap.set("#about", {  
+//     x: 500 
+// });
+
+// gsap.to("#about", {  
+//     duration: 2.2,
+//     x: 0,
+//     ease: "power2.inOut",
+//     yoyo: true,
+//     scrollTrigger: {
+//         trigger: "#about",
+//         start: "top center",
+//         end: "center",
+//         markers: false 
+//     } 
+// });
+
+gsap.to("progress", {
+    value: 100,
+    ease: "none",
+    scrollTrigger: {scrub: 0.3}
 });
 
-gsap.to("#about", {  
-    duration: 2.2,
-    x: 0,
-    ease: "power2.inOut",
-    yoyo: true,
-    scrollTrigger: {
-        trigger: "#about",
-        start: "top center",
-        end: "center",
-        markers: false 
-    } 
-});
