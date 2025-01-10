b348379435f0b2524757d568f20a9ea21664c3dd
bae111 <sunmoneee@gmail.com>
2025-01-10 20:29:05 +0900

    feat: hollys membership 작업

--- html/js/hollys.js
+++ html/js/hollys.js
@@ -1,6 +1,13 @@
 $(function() {
     $('#fullpage').fullpage({		      
-        anchors: ['sec1', 'sec2', 'sec3', 'sec4'],
+        anchors: ['sec1', 'sec2', 'sec3', 'sec4', 'sec5'],
         scrollingSpeed: 1000,
+        scrollOverflow: false,
+    });
+    $('.sec5 .right .m-info li img').hover(function() {
+        var newSrc = $(this).attr('src').replace('/renewal/hollys/logo-m-s-', '/renewal/hollys/logo-m-b-');
+        $('.sec5 .left img').attr('src', newSrc);
+    }, function() {
+        $('.sec5 .left img').attr('src', '/img/renewal/hollys/logo-m-b-r.png');
     });
 });
