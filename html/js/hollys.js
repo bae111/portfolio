c42d71d8330cfe46028ba7d002af4f4dc821e7e0
bae111 <sunmoneee@gmail.com>
2025-01-11 18:15:38 +0900

    feat: hollys sns 작업

--- html/js/hollys.js
+++ html/js/hollys.js
@@ -1,6 +1,6 @@
 $(function() {
-    $('#fullpage').fullpage({		      
-        anchors: ['sec1', 'sec2', 'sec3', 'sec4', 'sec5'],
+    $('#fullpage').fullpage({
+        anchors: ['sec1', 'sec2', 'sec3', 'sec4', 'sec5', 'sec6'],
         scrollingSpeed: 1000,
         scrollOverflow: false,
     });
