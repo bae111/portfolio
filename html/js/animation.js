c8a2c5d8ec859dd04cf6425037a3ff4642b8c2af
bae111 <sunmoneee@gmail.com>
2025-01-15 18:52:39 +0900

    feat: hollys animation 작업

--- html/js/animation.js
+++ html/js/animation.js
@@ -36,3 +36,122 @@ gsap.to(textSpans, {
         },
     },
 });
+
+function triggerGSAPAnimation(index) {
+    if (index === 1){ 
+        const tlSec2 = gsap.timeline();
+    
+        tlSec2.to(".con-title h2", {
+            duration: 0.8,
+            y: 0,              
+            opacity: 1,        
+            ease: "power2.out",
+            delay: 0.3         
+        })
+        .to(".con-title p", {  
+            duration: 0.6,
+            y: 0,
+            opacity: 1,
+            ease: "power2.out",
+            delay: 0        
+        }, "-=0.1")
+        .to(".info", {  
+            duration: 0.6,
+            x: 0,
+            opacity: 1,
+            ease: "power2.out",
+            delay: 0        
+        }, "-=0.2")
+        .to(".price-box", {  
+            duration: 0.6,
+            x: 0,
+            opacity: 1,
+            ease: "power2.out",
+            delay: 0,
+            onComplete: function() {
+                $(".price-box").addClass("animate");
+            }    
+        }, "-=0.3")
+        .to(".con-text-wrap .one", {  
+            duration: 0.6,
+            x: 0,
+            opacity: 1,
+            ease: "power2.out",
+            delay: 1, 
+        }, "-=1")
+        .to(".con-text-wrap .two", {  
+            duration: 0.6,
+            x: 0,
+            opacity: 1,
+            ease: "power2.out",
+            delay: 1, 
+        }, "-=1.1")
+        .to(".go-to", {  
+            duration: 0.6,
+            y: 0,
+            opacity: 1,
+            ease: "power2.out",
+            delay: 1, 
+        }, "-=1.2")
+        .to(".right .img", {  
+            duration: 0.6,
+            opacity: 1,
+            ease: "power2.out",
+            delay: 1.5, 
+        }, "-=1.7")
+        .to(".right .tel", {  
+            duration: 0.6,
+            y: 0,
+            opacity: 1,
+            ease: "power2.out",
+            delay: 1.5,       
+        }, "-=1.8");
+    }
+    if (index === 4){ 
+        const tlSec5 = gsap.timeline();
+    
+        tlSec5.to(".sec5 .text li", {
+            duration: 0.8,
+            y: 0,              
+            opacity: 1,        
+            ease: "power2.out",
+            delay: 0.3,
+            stagger: 0.2 
+        }, "-=0.1")
+        .to(".sec5 .left img", {  
+            duration: 0.6,
+            opacity: 1,
+            ease: "power2.out",
+            delay: 0        
+        }, "-=0.1")
+        .to(".sec5 .more", {  
+            duration: 0.6,
+            y: 0,     
+            opacity: 1,
+            ease: "power2.out",
+            delay: 0        
+        }, "-=0.1")
+        .to(".sec5 .title", {  
+            duration: 0.6,
+            x: 0,     
+            opacity: 1,
+            ease: "power2.out",
+            delay: 0        
+        }, "-=0.1")
+        .to(".sec5 .m-info img", {  
+            duration: 0.6,
+            opacity: 1,
+            ease: "power2.out",
+            delay: 0,
+            stagger: 0.2     
+        })
+        .to(".sec5 .info-wrap li", {  
+            duration: 0.8,
+            x: 0,              
+            opacity: 1,        
+            ease: "power2.out",
+            delay: 0.3,
+            stagger: 0.2      
+        }, "-=0.4");
+    }
+}
--- html/js/hollys.js
+++ html/js/hollys.js
@@ -3,11 +3,26 @@ $(function() {
         anchors: ['sec1', 'sec2', 'sec3', 'sec4', 'sec5', 'sec6', 'sec7', 'sec8'],
         scrollingSpeed: 1000,
         scrollOverflow: false,
+        afterLoad: function(anchorLink, index) {
+            // index가 객체일 경우 숫자로 변환
+            if (typeof index === 'object') {
+                index = index.index;
+            }
+            if (index === 7) {
+                $('#hollys-header').css('background', '#fff');
+            } else {
+                $('#hollys-header').css('background', '');
+            }
+            triggerGSAPAnimation(index);
+        }
     });
     $('.sec5 .right .m-info li img').hover(function() {
         var newSrc = $(this).attr('src').replace('/renewal/hollys/logo-m-s-', '/renewal/hollys/logo-m-b-');
-        $('.sec5 .left img').attr('src', newSrc);
+        $('.sec5 .left img').attr('src', newSrc).removeClass('flipped').addClass('flipped');
     }, function() {
-        $('.sec5 .left img').attr('src', '/img/renewal/hollys/logo-m-b-r.png');
+        $('.sec5 .left img').attr('src', '/img/renewal/hollys/logo-m-b-r.png').removeClass('flipped');
+    });
+    $('.sec5 .left img').on('animationend', function() {
+        $(this).removeClass('flipped');
     });
 });
